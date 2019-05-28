233c38673af3b41d241840537efe729e2d402d5b
Gordon Balaaka <balaakagordon@gmail.com>
2019-05-28 11:16:34 +0300

    re-implement signup

--- src/views/Registration.js
+++ src/views/Registration.js
@@ -4,26 +4,11 @@ import Header from '../components/Header';
 import Background from '../images/diary1.jpg';
 
 
-var sectionStyle = {
-    position: "fixed",
-    width: "100%",
-    height: "800px",
-    backgroundImage: `url(${Background})`,
-    backgroundPosition: 'center',
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-  };
-
-
-class Registration extends Component {
-	render() {
-		return (
-		<div style={sectionStyle}>
-			<Header />
-			<SignupForm />
-		</div>
-		)
-	}
-}
+const Registration = () => (
+	<div>
+		<Header />
+		<SignupForm />
+	</div>
+);
 
 export default Registration;
